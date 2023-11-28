4023436dcdbd6b21e2989cf5cc8e1786668e2313
Disperr <152178064+Disperr@users.noreply.github.com>
2023-11-28 16:52:48 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+﻿
+#include <windows.h>
+
+void main()
+{
+    HWND hWnd = GetForegroundWindow();
+    ShowWindow(hWnd, SW_HIDE);//Прячем консоль
+    //Светим меседжбокс
+    MessageBox(hWnd, L"Message text", L"Caption text", MB_OK);
+}
+
